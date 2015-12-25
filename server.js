90dee7bd6c1868f107d9b32f3c60f5891e1d9478
Shoma Nishikawa <shoma.nishikawa@gmail.com>
2015-12-25 12:56:53 -0500

    trying to add api

--- server.js
+++ server.js
@@ -1,20 +1,41 @@
-var express = require('express');
+var fs = require('fs');
 var path = require('path');
+var express = require('express');
 var webpack = require('webpack');
+var bodyParser = require('body-parser');
 var app = express();
+
+var COMMENTS_FILE = path.join(__dirname, 'comments.json');
 
 var isDevelopment = (process.env.NODE_ENV !== 'production');
 var static_path = path.join(__dirname, 'public');
 
-app.use(express.static(static_path))
-  .get('/', function (req, res) {
+app.use(express.static(static_path));
+
+app.get('/', function (req, res) {
     res.sendFile('index.html', {
       root: static_path
     });
-  }).listen(process.env.PORT || 8080, function (err) {
-    if (err) { console.log(err) };
-    console.log('Listening at localhost:8080');
+})
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/api/comments', function(req, res) {
+  fs.readFile(COMMENTS_FILE, function(err, data) {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    res.setHeader('Cache-Control', 'no-cache');
+    res.json(JSON.parse(data));
   });
+});
+
+app.listen(process.env.PORT || 8080, function (err) {
+  if (err) { console.log(err) };
+  console.log('Listening at localhost:8080');
+});
 
 if (isDevelopment) {
   var config = require('./webpack.config');
