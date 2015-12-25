0707566a2c70d83c6b32f0bf35763711d8269d48
Shoma Nishikawa <shoma.nishikawa@gmail.com>
2015-12-25 12:38:03 -0500

    adding more stuff and cleanup

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,10 +1,68 @@
 import React from 'react';
 
+const data = [
+  {id: 1, author: "Shoma Nishikawa", text: "This is one comment"},
+  {id: 2, author: "Jordan Walke", text: "This is *another* comment"}
+];
+
+export default class Comment extends React.Component {
+  render() {
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        {this.props.children}
+      </div>
+    );
+  }
+};
+
+export default class CommentList extends React.Component {
+  render() {
+    let commentNodes = this.props.data.map(function(comment) {
+      return (
+        <Comment author={comment.author} key={comment.id}>
+          {comment.text}
+        </Comment>
+      );
+    });
+    return (
+      <div className="commentList">
+        {commentNodes}
+      </div>
+    );
+  }
+}
+
+export default class CommentForm extends React.Component {
+  render() {
+    return (
+      <div className="commentForm">
+        Hello, world! I am a CommentForm.
+      </div>
+    )
+  }
+}
+
+export default class CommentBox extends React.Component {
+  render() {
+    return (
+      <div className="commentBox">
+        <h1>Comments</h1>
+        <CommentList data={this.props.data} />
+        <CommentForm />
+      </div>
+    )
+  }
+}
+
 export default class App extends React.Component {
   render() {
     return(
       <div>
         <h1>HOT TEST</h1>
+        <CommentBox data={data} />
       </div>
     )
   }
